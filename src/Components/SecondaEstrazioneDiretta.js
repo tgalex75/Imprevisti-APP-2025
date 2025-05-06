c3af99dd74a6ce004abb91c36dac488c5687a128
tgalex75 <81436086+tgalex75@users.noreply.github.com>
2025-05-06 22:35:23 +0200

    Tshirt color dynamic change

--- src/Components/SecondaEstrazioneDiretta.js
+++ src/Components/SecondaEstrazioneDiretta.js
@@ -1,18 +1,22 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React from "react";
-import firstkit from "../assets/imgs/firstKit.png";
-import gkKit from "../assets/imgs/gkKit.png";
+import { useContext } from "react";
+//import firstKit from "../assets/imgs/firstKit.png";
+//import gkKit from "../assets/imgs/gkKit.png";
+//import firstKit from "../assets/imgs/jersey.svg";
 import IndicatoreGiocatoriImpr from "./IndicatoreGiocatoriImpr";
+import JerseySVGBg from "./JerseySVGBg";
+import ColorContext from "../context/colorContext";
 
 const SecondaEstrazioneDiretta = (props) => {
-  const { numbExtrPlayer, extractedPlayer } = props;  
+  const { numbExtrPlayer, extractedPlayer } = props;
+  const { primary, secondary } = useContext(ColorContext);
 
   return (
     <section
       id="secEstrDiretta"
-      className="flex h-fit w-full flex-col items-center justify-around rounded-md border-2 border-gray-300/20 px-1 mb-12 md:min-h-[40%] xl:px-6"
+      className="mb-12 flex h-fit w-full flex-col items-center justify-around rounded-md border-2 border-gray-300/20 px-1 md:min-h-[40%] xl:px-6"
     >
-      <h4 className="my-1  xl:self-start text-xs font-bold uppercase text-gray-300 md:my-0 md:mb-1 md:text-lg">
+      <h4 className="my-1 text-xs font-bold uppercase text-gray-300 md:my-0 md:mb-1 md:text-lg xl:self-start">
         {numbExtrPlayer === 1 ? "Giocatore estratto" : "Giocatori estratti"}
       </h4>
       <main
@@ -21,23 +25,41 @@ const SecondaEstrazioneDiretta = (props) => {
       >
         <div
           id="extractedPlayers"
-          className="flex h-full flex-wrap items-center justify-end rounded-lg w-3/4 xl:w-1/2 md:flex-nowrap gap-2 xl:gap-4"
+          className="flex h-full w-3/4 flex-wrap items-center justify-end gap-2 rounded-lg md:flex-nowrap xl:w-1/2 xl:gap-4"
         >
           {extractedPlayer?.map((player, i) => {
             return (
               <div
                 key={i}
-                className="flex flex-col items-center justify-center overflow-hidden rounded bg-contain bg-center bg-no-repeat p-8 transition-all md:h-4/5 w-full"
-                style={{
+                className="relative flex w-full flex-col items-center justify-center overflow-hidden rounded transition-all md:h-4/5"
+                /* style={{
                   backgroundImage:
-                    player === 1 ? `url(${gkKit})` : `url(${firstkit})`,
-                }}
+                     `url(${<JerseySVGBg fillColor={primary} />})` : `url(${<JerseySVGBg fillColor={secondary} />})`,
+                }} */
               >
-                <span
-                  className={`block pb-5 font-['Anton'] text-4xl font-bold text-gray-300 ${numbExtrPlayer > 3 ? "pb-3 text-6xl xl:text-5xl" : "text-9xl xl:text-7xl"}`}
-                >
-                  {player}
-                </span>
+                {player === 1 ? (
+                  <>
+                    <JerseySVGBg
+                      fillColor={primary}
+                      strokeColor={secondary}
+                      className="stroke-2"
+                    />
+                    <span className={`absolute left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 font-['Anton'] text-slate-800 ${numbExtrPlayer > 3 ? "pb-4 text-7xl xl:text-8xl" : "pb-20 text-9xl xl:text-8xl"}`}>
+                      {player}
+                    </span>
+                  </>
+                ) : (
+                  <>
+                    <JerseySVGBg
+                      fillColor={secondary}
+                      strokeColor={primary}
+                      className="stroke-2"
+                    />
+                    <span className={`absolute left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 font-['Anton'] text-slate-800 ${numbExtrPlayer > 3 ? "pb-4 text-7xl xl:text-8xl" : "pb-20 text-9xl xl:text-8xl"}`}>
+                      {player}
+                    </span>
+                  </>
+                )}
               </div>
             );
           })}
--- src/Pages/EditorInterfaccia.js
+++ src/Pages/EditorInterfaccia.js
@@ -2,6 +2,7 @@ import React, { useContext } from "react";
 import ColorPickerComponent from "../Components/ColorPickerComponent";
 import { motion } from "framer-motion";
 import ColorContext from "../context/colorContext";
+import JerseySVGBg from "../Components/JerseySVGBg";
 
 const EditorInterfaccia = () => {
   const {
@@ -10,7 +11,7 @@ const EditorInterfaccia = () => {
     tertiary,
     updatePrimary,
     updateSecondary,
-    updateTertiary
+    updateTertiary,
   } = useContext(ColorContext);
 
   return (
@@ -22,9 +23,24 @@ const EditorInterfaccia = () => {
         transition={{ delay: 0.7, duration: 0.7 }}
         className="h-full w-full items-center justify-around gap-2 overflow-hidden rounded-lg bg-black/50 p-4 text-gray-300 xl:flex"
       >
-        <ColorPickerComponent defaultColor={primary} updateFunc={updatePrimary} />
-        <ColorPickerComponent defaultColor={secondary} updateFunc={updateSecondary} />
-        <ColorPickerComponent defaultColor={tertiary} updateFunc={updateTertiary} />
+        <ColorPickerComponent
+          defaultColor={primary}
+          updateFunc={updatePrimary}
+        />
+        <ColorPickerComponent
+          defaultColor={secondary}
+          updateFunc={updateSecondary}
+        />
+        <ColorPickerComponent
+          defaultColor={tertiary}
+          updateFunc={updateTertiary}
+        />
+        <section className="relative flex h-48 w-40 flex-col items-center justify-center border">
+          <JerseySVGBg fillColor={primary} strokeColor={secondary} className="stroke-2" />
+          <span className="absolute left-1/2 top-1/2 z-50 -translate-x-1/2 -translate-y-1/2 text-9xl text-white">
+            4
+          </span>
+        </section>
       </motion.div>
     </section>
   );
